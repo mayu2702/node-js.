85cc2b3839042323f34d77602372db4873ba363b
Mayur Bakal <131716114+mayu2702@users.noreply.github.com>
2024-06-21 18:54:24 +0000

    container config done

---
+++ basics/Basics js/variables.js
@@ -0,0 +1,14 @@
+const accountID = 12345
+let accountEmail = "mayu12@gmail.com"
+var accountPassword = "2373294"
+accountCity = "Pune" 
+let accountState;
+
+
+
+accountEmail = "max12@gmail.com"
+accountPassword = " 126428"
+accountCity = "chennai"
+
+console.log(accountID);
+console.table([accountEmail,accountID,accountPassword,accountCity,accountState]);
