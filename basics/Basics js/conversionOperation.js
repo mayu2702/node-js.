0dc3ddf0783f4db8b3d93e440974250ffbe19ffb
Mayur Bakal <131716114+mayu2702@users.noreply.github.com>
2024-06-21 19:47:35 +0000

    conatainer config done

---
+++ basics/Basics js/conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "33aas";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number (score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+
+// 33 -= 33 
+//33abc -= NaN
+// true = 1; flase = 0
