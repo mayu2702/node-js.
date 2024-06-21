cbf5161111587669eda034185eb6c42e167b11a5
Mayur Bakal <131716114+mayu2702@users.noreply.github.com>
2024-06-21 19:27:33 +0000

    container config done

---
+++ basics/Basics js/Datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all the js code as newer version 
+
+//alert(3+4)   //we are using the nodejs not the browser 
+
+number 
+BigInt
+Boolean
+Object
+String
+null
+undefined
+Symbol
