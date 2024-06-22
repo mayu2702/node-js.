9dc1041de5e3a5e74fead2fbe7ab520c97f3fb1b
Mayur Bakal <131716114+mayu2702@users.noreply.github.com>
2024-06-22 11:19:06 +0000

    container config done

--- basics/Basics js/DatatypesSummary.js
+++ basics/Basics js/DatatypesSummary.js
@@ -23,3 +23,32 @@ const myobj = {
 const myfunction = function(){
     console.log("Hello wolrd");
 }
+
+
+
+//////  ++++++++++++++++++++++++++++++++++=MEOMARY
+
+// STACK = ( primitive datatypes) , Heap = ( non primitive datatypes)
+let myyoutubename = "mayurbakal"
+
+let anothername = myyoutubename
+anothername = "chai aur code"
+
+console.log(myyoutubename);
+console.log(anothername);
+
+
+//heap
+
+let userOne ={
+    email : "mayur@google.com",
+    upi: "mayur@ybl"
+} 
+
+let userTwo = userOne
+userTwo.email = "johny@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
