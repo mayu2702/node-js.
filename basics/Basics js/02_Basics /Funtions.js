1760758592747f02f6260561af05f3ef64e11126
Mayur Bakal <131716114+mayu2702@users.noreply.github.com>
2024-06-27 11:43:13 +0000

    container config done

--- basics/Basics js/02_Basics /Funtions.js
+++ basics/Basics js/02_Basics /Funtions.js
@@ -31,4 +31,32 @@ function LoginUserMessage(username ="sam")
 }
 
 console.log(LoginUserMessage("Mayur"))
-console.log(LoginUserMessage())    //undefined 
+console.log(LoginUserMessage())    //undefined
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+//console.log(calculateCartPrice(200,400,500,4894,3447));
+
+const user = {
+    username : "Mayur",
+    price : 199
+
+}
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleobject(user)
+handleobject({
+    username : "sam",
+    price : 399
+})
+
+const myNewArray = [300,300,3992,200,100]
+
+function retunSecondValue (getArray){
+    return getArray[0]
+}
+console.log(retunSecondValue(myNewArray));
+console.log(retunSecondValue([200,345,456,5678]));
