4bd98948f81279e329c5c3e2b333866faa200631
Mayur Bakal <131716114+mayu2702@users.noreply.github.com>
2024-06-27 20:00:50 +0000

    container config done

--- basics/Basics js/02_Basics /Scope.js
+++ basics/Basics js/02_Basics /Scope.js
@@ -13,3 +13,40 @@ for( let i = 0; i< array.length;i++){
 //console.log(b); 
 console.log(a);  //300
 console.log(c);   //scope problem with the var 
+
+
+function one (){
+    const username  ="Mayur"
+
+    function two(){
+        const website ="github"
+        console.log(username);
+    }
+    //console.log(website);error
+
+    two()
+}
+one()
+
+if(true){
+    const username = "Mayur"
+    if(username == "Mayur"){
+        const website = "github"
+        console.log(username + website);
+    }
+    //console.log(website);  error
+}
+//console.log(username);error
+
+// +++++++++++++Interesting+++++++++++++
+
+function addone (Num){
+    return Num+1
+
+}
+console.log(addone(5))
+
+const addTwo = function(Num){
+    return Num + 2
+}
+addTwo(5)
